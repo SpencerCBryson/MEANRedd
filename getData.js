3a0f6d996196603e1d6f2d072ca910d24dcc35e5
Spencer Bryson <spencer.bryson@uoit.net>
2018-03-18 17:54:56 -0400

    blah

--- getData.js
+++ getData.js
@@ -2,34 +2,37 @@ var redditURL = "https://www.reddit.com/r/";
 var newParams = "/new.json?limit=100";
 var topParams = "/top.json?limit=100&t=month";
 //var str_ = "https://www.reddit.com/r/" + subreddit + "/new.json?limit=100"
-var titles = [];
+//var titles = [];
 
 var ws_pattern = /\w+'*\w+/g
 
-var data = []
+var data = {}
+var cookedData = {}
+
+var total_iterations = 0
+var max_iterations = 0
+var maxListings = 10
 
 function postData(post) {
     let id_ = post.data.id
     let title_ = post.data.title.toLowerCase()
-    var content_ = post.data.selftext //need to clean content
-    var words = null;
+    var content_ = post.data.selftext
+    let score_ = post.data.score
+    let reddit_ = post.data.subreddit
+    var wordScores_, words = null
     
     content_ = content_.toLowerCase()
-    let score_ = post.data.score
-    
-    var wordScores_;
-
-    var words = null;
     
     if(content_) {
-        wordsArr = content_.replace(/[,./?!()*]/g, '').split(' ');
-        words = wordsArr.filter(word => !word.match(/[^a-zA-Z]/g));
+        //if the post contains a block of post content
+        wordsArr = content_.replace(/[,./?!()*]/g, '').split(' ')
+        words = wordsArr.filter(word => !word.match(/[^a-zA-Z]/g))
     }
     else if (title_) {
-        wordsArr = title_.replace(/[,./?!()*]/g, '').split(' ');
-        words = wordsArr.filter(word => !word.match(/[^a-zA-Z]/g));
-
-        // console.log(words); console.log(title_);
+        //if the post has no text content (i.e. a link or video)
+        //will have to base the score on the title
+        wordsArr = title_.replace(/[,./?!()*]/g, '').split(' ')
+        words = wordsArr.filter(word => !word.match(/[^a-zA-Z]/g))
     }
 
     words = words.filter(word => !stop_words.has(word) && isNaN(word))
@@ -42,81 +45,120 @@ function postData(post) {
     let uWords = Object.keys(counts)
     let scorePerWord = score_ / uWords.length
 
-    if(content_) {
+    if(content_)
         wordScores_ = uWords.map(function(x, i){return {"word": x, "score": counts[x] + 0.2 * scorePerWord}})
-        // console.log(wordScores_)
-    } else if (title_) {
+    else if (title_)
         wordScores_ = uWords.map(function(x, i){return {"word": x, "score": counts[x] + 0.02 * scorePerWord}})
-        // console.log(wordScores_)
-    }
-    
 
     return {
         id: id_,
         title: title_,
         score: score_,
         content: content_,
-        wordScores: wordScores_
+        reddit: reddit_,
+        wordScores: wordScores_,
+        wordCounts: counts,
     };
 }
 
-function callback(result) {
+function addData(result, subreddit) {
     //console.log(result)
 
     var posts = result.data.children
     
-    for(post of posts) {
-        titles.push(post.data.title)
-        data.push(postData(post))
-    }
+     if(!data[subreddit])
+        data[subreddit] = []
+    
+    for(post of posts)
+        data[subreddit].push(postData(post))
 
     return result.data.after
 }
 
-function summarize() {
-    console.log(data.length)
-
-    var wordScoresByPost = data.map(post => post.wordScores);
-    wordScoresByPost = wordScoresByPost.map(function(x) { if (x) return x; else return []; })
-    var wordScores = new Map();
+function summarizeSubReddit(subreddit, iter) {
+    let sr_data = data[subreddit];
+        
+    var wordScoresByPost = sr_data.map(post => post.wordScores);
+    wordScoresByPost = wordScoresByPost.map(function(x) {
+        if (x) return x; else return []; 
+    })
+    
+    var wordScores = {}
     
     wordScoresByPost.reduce((a, b) => a.concat(b)).map(
         function (word)  {
-            if (wordScores[word.word]) wordScores[word.word] += word.score;
-            else wordScores[word.word] = word.score;
+            if (wordScores[word.word]) 
+                wordScores[word.word] += word.score;
+            else 
+                wordScores[word.word] = word.score;
         }
     )
     
-    var topWords = Object.keys(wordScores);
-    topWords.sort((a, b) => wordScores[b] - wordScores[a]);
-    
-    //topWords.slice(0, 10).map(word => console.log(word + ": " + wordScores[word]))
-    let result = topWords.slice(0, 10)
 
-    display(result,wordScores)
+    let wordData = {}
+    
+    wordData["wordScores"] = wordScores;
+    //wordData["wordCounts"] = wordCounts;
+    
+    cookedData[subreddit] = wordData
+    summarize(iter)
 }
 
-var total_iterations = 0
-var max_iterations = 0
-var maxListings = 100
+function summarize(iter) {
+    if(iter != max_iterations)
+        return
+        
+    let subreddits = Object.keys(data)
+    
+    var combined = {
+        wordScores : {},
+        wordCounts : {}
+    }
+    
+    for(sr of subreddits) {
+        let wordScores = cookedData[sr].wordScores
+        
+        Object.keys(cookedData[sr].wordScores).forEach(function(word) {
+            if(combined.wordScores[word])
+                combined.wordScores[word] += wordScores[word]
+            else
+                combined.wordScores[word] = wordScores[word]
+        });
+    }
 
-function getPosts(str, next, i) {
-    var url = str;
+    
+    //cookedData["combinedData"] = combined;
+    
+    var words = Object.keys(combined.wordScores);
+    let result = words.sort((a, b) => combined.wordScores[b] - combined.wordScores[a]);
+    
+    display(result.slice(0, 20), cookedData, combined)
+    
+} 
 
+function getPosts(subreddit, params, next, i) {
+    var url = redditURL + subreddit + params;
+    
     if (next) url += "&after=" + next;
 
     $.getJSON(url, i, function(result) {
-        console.log(result)
-        let next_ = callback(result)
+        //console.log(result)
         
-        //console.log(total_iterations)
-        total_iterations++
-        updateCount(total_iterations, max_iterations)
-        i++
+        //append data to our list and fetch the
+        // ID of the next listing
+        let next_ = addData(result,subreddit)
 
-        if(total_iterations == max_iterations) { summarize() }
-        else if (i < maxListings && next_) {
-            getPosts(str, next_, i);
-        }
+        //increase counters to keep track of our progress
+        total_iterations++
+        i++
+        
+        //display scraping progress to user
+        updateCount(total_iterations, max_iterations)
+
+        if (i < maxListings && next_) 
+            //keep fetching posts if there is more
+            getPosts(subreddit, params, next_, i);
+        else
+            summarizeSubReddit(subreddit, total_iterations)
     });
 }
